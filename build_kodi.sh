da9416b62fc8110f52c7168b9f8b199e09a6c74a
alekseyzimin <alekseyz@genome5.umd.edu>
2019-04-23 17:15:09 -0400

    added build kodi

---
+++ build_kodi.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+(cd build && make clean)
+(cd tools/depends/ && \
+./bootstrap && \
+./configure --with-tarballs=$HOME/android-tools/xbmc-tarballs --host=arm-linux-androideabi --with-sdk-path=$HOME/android-tools/android-sdk-linux --with-ndk-path=$HOME/android-tools/android-ndk-r18 --with-toolchain=$HOME/android-tools/arm-linux-androideabi-vanilla/android-21 --prefix=$HOME/android-tools/xbmc-depends && \
+make -j 4)
+
+make -C tools/depends/target/cmakebuildsys/ 
+
+(cd build && make -j 4 && make apk)
