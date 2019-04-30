33f9ba612ab00796d3276bd7a4869c01686b00a5
alekseyzimin <alekseyz@genome5.umd.edu>
2019-04-30 12:02:58 -0400

    updated build kodi script

--- build_kodi.sh
+++ build_kodi.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-(cd build && make clean)
+(cd build && make distclean)
+(cd tools/depends/ && make distclean)
 (cd tools/depends/ && \
 ./bootstrap && \
-./configure --with-tarballs=$HOME/android-tools/xbmc-tarballs --host=arm-linux-androideabi --with-sdk-path=$HOME/android-tools/android-sdk-linux --with-ndk-path=$HOME/android-tools/android-ndk-r18 --with-toolchain=$HOME/android-tools/arm-linux-androideabi-vanilla/android-21 --prefix=$HOME/android-tools/xbmc-depends && \
+./configure --enable-debug=no --with-tarballs=$HOME/android-tools/xbmc-tarballs --host=arm-linux-androideabi --with-sdk-path=$HOME/android-tools/android-sdk-linux --with-ndk-path=$HOME/android-tools/android-ndk-r18 --with-toolchain=$HOME/android-tools/arm-linux-androideabi-vanilla/android-21 --prefix=$HOME/android-tools/xbmc-depends && \
 make -j 4)
+
+make -j 4 -C tools/depends/target/binary-addons
 
 make -C tools/depends/target/cmakebuildsys/ 
 
